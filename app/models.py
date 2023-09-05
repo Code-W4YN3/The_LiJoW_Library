f642a0d46cab16352d93e544b0a20fb1b7ee0c58
Joanne Ndirangu <joannendirangu15@gmail.com>
2023-09-05 13:08:52 +0300

    Added Book, Reader and Review tables

--- app/models.py
+++ app/models.py
@@ -6,3 +6,28 @@ Base = declarative_base()
 engine = create_engine('sqlite:///library.db')
 Session = sessionmaker(bind=engine)
 session = Session()
+
+class Book(Base):
+    __tablename__ = 'books'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    author = Column(String())
+    genre = Column(String())
+    reader_count = Column(Integer())
+
+class Reader(Base):
+    __tablename__ = 'readers'
+
+    id = Column(Integer(), primary_key=True)
+    first_name = Column(String())
+    last_name = Column(String())
+    library_id = Column(Integer())
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer(), primary_key=True)
+    rating = Column(Integer())
+    book_id = Column(Integer())
+    reader_id = Column(Integer())

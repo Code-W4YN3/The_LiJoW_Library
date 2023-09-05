0c60ecd8e60bdce8bba77cd53867c51dbee06863
Linet <muthiigathoni@gmail.com>
2023-09-05 23:25:57 +0300

    Seed initial data

--- app/seed.py
+++ app/seed.py
@@ -8,7 +8,39 @@ from models import Book, Reader, Review, engine, session
 fake = Faker()
 
 if __name__ == '__main__':
-    pass
+    def seed_data():
+        for _ in range(20):
+            book = Book(
+                title=fake.catch_phrase(),
+                author=fake.name(),
+                publication_year=fake.random_int(min=1900, max=2023),
+            )
+            session.add(book)
 
+        for _ in range(20):
+            reader = Reader(
+                name=fake.name(),
+                email=fake.email(),
+                birthdate=fake.date_of_birth(minimum_age=18, maximum_age=80),
+            )
+            session.add(reader)
 
-session.close()
+        # Create random reviews for the books
+        books = session.query(Book).all()
+        readers = session.query(Reader).all()
+        for _ in range(30):
+            book = random.choice(books)
+            reader = random.choice(readers)
+            review = Review(
+                rating=random.randint(1, 5),
+                comments=fake.paragraph(),
+                book=book,
+                reader=reader,
+            )
+            session.add(review)
+
+    seed_data()
+
+    session.commit()
+
+    session.close()

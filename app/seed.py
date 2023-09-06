4751694c1e7b33a919a651aa70542561b3adc04a
Linet <muthiigathoni@gmail.com>
2023-09-06 09:15:12 +0300

    Seed initial data

--- app/seed.py
+++ app/seed.py
@@ -1,46 +1,48 @@
 from faker import Faker
 import random
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
-from models import Book, Reader, Review, engine, session
+from models import Book, Reader, Review, engine, Base
 
 fake = Faker()
 
-if __name__ == '__main__':
-    def seed_data():
-        for _ in range(20):
-            book = Book(
-                title=fake.catch_phrase(),
-                author=fake.name(),
-                publication_year=fake.random_int(min=1900, max=2023),
-            )
-            session.add(book)
+def seed_data():
+    Base.metadata.create_all(engine)
 
-        for _ in range(20):
-            reader = Reader(
-                name=fake.name(),
-                email=fake.email(),
-                birthdate=fake.date_of_birth(minimum_age=18, maximum_age=80),
-            )
-            session.add(reader)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    
+    for _ in range(10): 
+        book = Book(
+            name=fake.unique.first_name(),
+            author=fake.name(),
+            genre=fake.word(),
+            reader_count=random.randint(1, 100)
+        )
+        session.add(book)
 
-        # Create random reviews for the books
-        books = session.query(Book).all()
-        readers = session.query(Reader).all()
-        for _ in range(30):
-            book = random.choice(books)
-            reader = random.choice(readers)
-            review = Review(
-                rating=random.randint(1, 5),
-                comments=fake.paragraph(),
-                book=book,
-                reader=reader,
-            )
-            session.add(review)
-
-    seed_data()
-
+    for _ in range(20): 
+        reader = Reader(
+            first_name=fake.first_name(),
+            last_name=fake.last_name(),
+            library_id=fake.unique.random_number()
+        )
+        session.add(reader)
+   
+    books = session.query(Book).all()
+    readers = session.query(Reader).all()
+    for _ in range(30):  
+        book = random.choice(books)
+        reader = random.choice(readers)
+        review = Review(
+            rating=random.randint(1, 5),
+            book=book,
+            reader=reader
+        )
+        session.add(review)
+    
     session.commit()
-
     session.close()
+
+
+if __name__ == '__main__':
+    seed_data()
